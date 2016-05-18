a68de366301d0868ecc9209ed4d1a78730224516
Robert Kolozsi <robert.kolozsi@gmail.com>
2016-05-18 23:33:01 +0200

    Added palindrom.py exercise.

---
+++ palindrome.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+
+def palindrome(st):
+    if st == st[::-1]:
+        return "String: <" + st + "> is palindrome!"
+    else:
+        return "String: <" + st + "> is NOT palindrome!"
+
+
+def main(st):
+    print palindrome(st)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='String to be checked if it is palindrome.')
+    parser.add_argument('string', nargs='?')
+    arg = parser.parse_args()
+    if arg.string:
+        main(arg.string)
+    else:
+        st = raw_input("Type any string: ")
+        print palindrome(st)
