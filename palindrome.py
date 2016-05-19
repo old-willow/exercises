deda728950588d2f507c52eb4579f5036e56c65d
Robert Kolozsi <robert.kolozsi@gmail.com>
2016-05-19 09:02:32 +0200

    Added palindrome_manual function for palindrome.py file.

--- palindrome.py
+++ palindrome.py
@@ -11,8 +11,22 @@ def palindrome(st):
         return "String: <" + st + "> is NOT palindrome!"
 
 
+def palindrome_manual(st):
+    reverse_st = ''
+    st_len = len(st)
+
+    for c in range(1, st_len + 1):
+        reverse_st += st[st_len - c]
+
+    if st == reverse_st:
+        return "String: <" + st + "> is palindrome!"
+    else:
+        return "String: <" + st + "> is NOT palindrome!"
+
+
 def main(st):
-    print palindrome(st)
+    #print palindrome(st)
+    print palindrome_manual(st)
 
 
 if __name__ == '__main__':
@@ -23,4 +37,5 @@ if __name__ == '__main__':
         main(arg.string)
     else:
         st = raw_input("Type any string: ")
-        print palindrome(st)
+        #print palindrome(st)
+        print palindrome_manual(st)
